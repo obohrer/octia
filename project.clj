4c0a0103c6fe2e3b9820c897dcf776e1a787c066
Olivier Bohrer <olivier.bohrer@smallrivers.com>
2013-03-02 17:50:34 +0100

    project : add description

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject octia "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A clojure library on top of compojure/ring to easily define api endpoints.
+  Support iodocs generation."
+  :url "http://github.com/obohrer/octia"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure           "1.1.1"]
                  [cheshire            "5.0.1"]]
+  :warn-on-reflection true
   :profiles {:dev
-             {:dependencies [[ring-mock       "0.1.1"]
-                             [midje           "1.3.0" :exclusions [org.clojure/clojure]]]}})
+             {:dependencies [[ring-mock "0.1.1"]
+                             [midje     "1.3.0"
+                              :exclusions [org.clojure/clojure]]]}})
