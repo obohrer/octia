7a6a43b652b96cdf9caebefdfd8b3d33f72b7458
Reynald Borer <reynald.borer@paper.li>
2014-04-15 13:39:37 +0200

    Bump to clojure 1.5.1 (and upgrade deps)

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject octia "0.0.2"
+(defproject octia "0.0.3"
   :description "A clojure library on top of compojure/ring to easily define api endpoints.
   Supports iodocs generation."
   :url "http://github.com/obohrer/octia"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure           "1.1.1"]
-                 [cheshire            "5.0.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure           "1.1.6"
+                  :exclusions [joda-time]]
+                 [cheshire            "5.3.1"]]
   :warn-on-reflection true
   :profiles {:dev
-             {:dependencies [[ring-mock "0.1.1"]
-                             [midje     "1.3.0"
-                              :exclusions [org.clojure/clojure]]]}})
+             {:dependencies [[ring-mock "0.1.5"]
+                             [midje     "1.4.0"
+                              :exclusions [org.clojure/clojure org.clojure/tools.macro joda-time]]]}})
